4b34d5a7e6c0d02a19a688f8fc2e5197a4c54161
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-09 19:49:32 +0700

    update head

--- docs/_next/static/chunks/2b070c6768cd6a16.js
+++ docs/_next/static/chunks/2b070c6768cd6a16.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/20d541f8328b0226.js",
+  "static/chunks/59577e95574bcd94.js",
   "static/chunks/be0a1f7bd72728d8.js",
   "static/chunks/e33860403873cb07.js",
   "static/chunks/fe2987111908a487.css",
   "static/chunks/be66d72b0409bf44.css",
-  "static/chunks/turbopack-b4035a2be85c14a5.js"
+  "static/chunks/turbopack-2630eca6c493b4f4.js"
 ])
