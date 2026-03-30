dc6976814abd47383c7d8dcb6a128d9907b778f3
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-30 20:49:23 +0700

    Updatescripts

--- docs/_next/static/chunks/123-ebpay~b_u.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/500", [
-  "static/chunks/13eq65qrc.w2k.js",
-  "static/chunks/0aanxgxr-gzts.js",
-  "static/chunks/0i2jozeo_3fl~.js",
-  "static/chunks/0xrk84cr-1.ds.js",
-  "static/chunks/turbopack-0cvlfa0ktutzz.js"
-])
---
+++ docs/_next/static/oGqfS36TeS5y3hGfzWz9t/_buildManifest.js
@@ -0,0 +1,42 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0sbzdo7l52sme.js"
+  ],
+  "/404": [
+    "static/chunks/0i828gnfce.6d.js"
+  ],
+  "/500": [
+    "static/chunks/0pr4qszmr5j7x.js"
+  ],
+  "/_error": [
+    "static/chunks/0fgnj5ldpkw0o.js"
+  ],
+  "/app": [
+    "static/chunks/11lkkids9af.d.js"
+  ],
+  "/app/editor": [
+    "static/chunks/0zvaz0yw10fj9.js"
+  ],
+  "/app/icons": [
+    "static/chunks/0f75d_lhrw.sr.js"
+  ],
+  "/version": [
+    "static/chunks/0sv04rer9m.o0.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/500",
+    "/_app",
+    "/_error",
+    "/app",
+    "/app/editor",
+    "/app/icons",
+    "/version"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
